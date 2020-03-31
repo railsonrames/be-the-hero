1de3c459638b34057580646f15a0d99d4e1ff7bf
Railson Rames <rames.dev@outlook.com>
2020-03-31 15:33:12 -0300

    Added CORS.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,15 +3,21 @@ const dbconnection = require('../database/dbconnection');
 module.exports = {
   async index(request, response) {
     const { page = 1 } = request.query;
-
     const [count] = await dbconnection('incidents').count();
-
     response.header('X-Total-Count', count['count(*)']);
 
     const incidents = await dbconnection('incidents')
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
       .limit(5)
       .offset((page - 1) * 5)
-      .select('*');
+      .select([
+        'incidents.*',
+        'ongs.name',
+        'ongs.email',
+        'ongs.whatsapp',
+        'ongs.city',
+        'ongs.uf'
+      ]);
 
     return response.json(incidents);
   },
